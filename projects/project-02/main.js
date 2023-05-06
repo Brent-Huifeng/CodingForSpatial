1b67b8890f087a42d547b4331c2499cb12d4527c
Brent-Huifeng <124107748+Brent-Huifeng@users.noreply.github.com>
2023-05-06 16:56:31 -0400

    1

--- projects/project-02/main.js
+++ projects/project-02/main.js
@@ -610,3 +610,64 @@ function filterFn(event) {
   }
 }
 filterBtns.addEventListener("click", filterFn);
+
+
+
+
+
+
+
+
+/* 
+
+Instructions: when a user clicks on one of the colored dots, the    background color of the entire page changes to match that dot. You should not need to change any HTML or CSS. 
+
+Hint: Classes for each color have already been defined in the CSS tab.
+
+*/
+
+// 1 -- Select each button
+
+let Btn1 = document.getElementById("Button1");
+let whiteBtn = document.getElementById("Button2")
+let blueBtn = document.getElementById("Button3");
+let yellowBtn = document.getElementById("Button4");
+let Btn5 = document.getElementById("Button5");
+
+// 3 -- Write an event handler to change the color of the page when each circle is pressed.
+
+function swithcTo1() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "#a3f2cc";
+    // body.style.color = "#a3f2cc";
+  }
+  
+  function switchToWhite() {
+      let body = document.querySelector("body");
+      body.style.backgroundColor = "#f4d35b";
+      // body.style.color = "#f4d35b";
+    }
+  
+  function switchToBlue() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "#ddb9fe";
+    // body.style.color = "#ddb9fe";
+  }
+  
+  function switchToYellow() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "#b9f133";
+    // body.style.color = "#b9f133";
+  }
+
+  function switchTo5() {
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "#ffffff";
+    // body.style.color = "#b9f133";
+  }
+  // 2 -- Add an event listener to each circle
+  Btn1.addEventListener("click", swithcTo1);
+  whiteBtn.addEventListener("click", switchToWhite);
+  blueBtn.addEventListener("click", switchToBlue);
+  yellowBtn.addEventListener("click", switchToYellow);
+  Btn5.addEventListener("click", switchTo5);
